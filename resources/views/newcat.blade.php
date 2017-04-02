ac4579314a40b58466da6bd2475dc243681d481d
codobred <codobred@gmail.com>
2017-04-02 10:55:07 +0300

    change "add category page"

--- resources/views/newcat.blade.php
+++ resources/views/newcat.blade.php
@@ -1,13 +1,33 @@
 @extends('app')
 
 @section('content')
-    <form method="POST" action="http://test1.a2-lab.com/category" accept-charset="UTF-8">
-        {!! csrf_field() !!}
-        <input name="user_id" type="hidden" value="1">
-        <label for="category_name">Category name: </label>
-        <input name="category_name" type="text" id="category_name"><br>
-        <input type="submit" value="Add new category">
-    </form>
+	<div class="row">
+	    <div class="col-lg-12">
+	        <h3 class="page-header">
+	        	<i class="fa fa-plus"></i> Add new category
+	        </h3>
+	    </div>
+	    <!-- /.col-lg-12 -->
+	</div>
+
+	<div class="row">
+		<div class="col-lg-12">
+			<form method="POST" action="http://test1.a2-lab.com/category" accept-charset="UTF-8">
+				{!! csrf_field() !!}
+				<input name="user_id" type="hidden" value="1">
+
+				<div class="form-group input-group">
+                    <input name="category_name" type="text" placeholder="Category name" class="form-control">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default" type="submit">
+                        	<i class="fa fa-plus"></i> Add new
+                        </button>
+                    </span>
+                </div>
+			</form>
+		</div>
+	</div>
 
     @include('layouts.errors', ['submitButton' => 'Add new category'])
-@stop
+
+@endsection
