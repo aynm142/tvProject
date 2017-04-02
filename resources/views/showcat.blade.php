cf038675fb7431f6a4152ec203b87c4ea08b83bd
codobred <codobred@gmail.com>
2017-04-02 10:37:06 +0300

    change "show categories" view

--- resources/views/showcat.blade.php
+++ resources/views/showcat.blade.php
@@ -1,12 +1,40 @@
 @extends('app')
 @section('content')
-    @foreach($categories as $key => $category)
-        <h4>{{ $category->category_name }}</h4>
-        <a href="{{ URL::to('/category/' . $category->id . '/edit') }}">Edit</a>
-        {{ Form::open(array('url' => 'category/' . $category->id)) }}
-        {{ Form::hidden('_method', 'DELETE') }}
-        {{ Form::submit('Delete') }}
-        {{ Form::close() }}
-    @endforeach
+	<div class="row">
+	    <div class="col-lg-12">
+	        <h1 class="page-header">All Categories</h1>
+	    </div>
+	    <!-- /.col-lg-12 -->
+	</div>
+	<div class="row">
+		<div class="col-lg-12">
+	        <div class="panel panel-default panel-categories">
+	            <div class="panel-heading">
+	                Manage the categories below:
+	            </div>
+	            <!-- .panel-heading -->
+	            <div class="panel-body">
+	                <ul class="list-group">
+	                	@foreach($categories as $key => $category)
+	                		<li class="list-group-item">
+						        {{ $category->category_name }}
 
-@stop
+						        <div class="action-buttons">
+						            <a class="btn btn-sm btn-default pull-left m-r-5" href="{{ URL::to('/category/' . $category->id . '/edit') }}">Edit</a>
+									<form class="pull-left" action="{{ 'category/' . $category->id }}" method="POST">
+										{{ csrf_field() }}
+										<input type="hidden" name="_method" value="DELETE">
+										<button type="submit" class="btn btn-sm btn-danger">Delete</button>
+									</form>
+						        </div>
+						    </li>
+					    @endforeach
+					</ul>
+	            </div>
+	            <!-- .panel-body -->
+	        </div>
+	        <!-- /.panel -->
+	    </div>
+	</div>
+
+@endsection
