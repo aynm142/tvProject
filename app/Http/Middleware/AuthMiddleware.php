f02dffed8fb1f0c65182088d0ceeed2f09cc84b3
defunktt1 <vurado3@gmail.com>
2017-04-05 12:06:10 +0300

    cat api middleware

---
+++ app/Http/Middleware/AuthMiddleware.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use App\User;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class AuthMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+//        for process request, must exists user
+        try {
+//            dd(User::where('device_token', $request->device_token)->firstOrFail());
+            $user = User::where('device_token', $request->device_token)->firstOrFail();
+        } catch (ModelNotFoundException $exception) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+        $request['user_id'] = $user->id;
+
+        return $next($request);
+    }
+}
