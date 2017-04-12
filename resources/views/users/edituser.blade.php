f7b6c601d2ce9d9c134eccfa943fb3dc4e6ff9fe
defunktt1 <vurado3@gmail.com>
2017-04-12 20:19:23 +0300

    Edit and add new user

--- resources/views/users/edituser.blade.php
+++ resources/views/users/edituser.blade.php
@@ -3,11 +3,11 @@
     @include('layouts.errors')
     {!! Form::model($user, ['method' => 'PATCH', 'action' => ['UserController@update', $user->id]]) !!}
     {!! Form::label('name', 'User name:') !!}
-    {!! Form::text('name', null, ['id' => 'name']) !!}
+    {!! Form::text('name', null, ['id' => 'name']) !!}<br>
     {!! Form::label('email', 'User email:') !!}
-    {!! Form::text('email', null, ['id' => 'email']) !!}
+    {!! Form::text('email', null, ['id' => 'email']) !!}<br>
     {!! Form::label('password', 'User password: ((leave empty to keep your original password) [[[ Create a new help field for this :D ]]]') !!}
-    {!! Form::password('password') !!}
+    {!! Form::password('password') !!}<br>
     {!! Form::label('admin', 'Admin: ') !!}
     {!! Form::select('is_admin', ['No', 'Yes']) !!}<br>
     {{ Form::submit('Save') }}
