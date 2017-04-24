d84cfac18ac53599418c1e5449bbc64ccc0b53cc
codobred <codobred@gmail.com>
2017-04-24 09:17:17 +0300

    update settings view

--- resources/views/settings/settings.blade.php
+++ resources/views/settings/settings.blade.php
@@ -1,15 +1,67 @@
 @extends('app')
 
 @section('content')
-    {!! Form::model($data, ['method' => 'PATCH', 'files' => true, 'action' => ['SettingsController@update', $data]]) !!}
-    {!! Form::label('site_name', 'Site name:') !!}
-    {!! Form::text('site_name', null) !!}<br>
-    {!! Form::label('site_name', 'Logo image:') !!}
-    {!! Form::text('logo_image', null) !!}<br>
-    {!! Form::label('background_image', 'Background image:') !!}
-    {!! Form::text('background_image', null) !!}<br>
-    {!! Form::label('banner', 'Banner on\off') !!}
-    {!! Form::select('banner', ['No', 'Yes'], null, ['required' => 'required']) !!}
-    {!! Form::submit() !!}
-    {!! Form::close() !!}
+    <div class="row">
+        <div class="col-lg-12">
+            <h3 class="page-header">
+                <i class="fa fa-gear"></i> Site Settings
+            </h3>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+
+    @include('layouts.errors')
+
+    <div class="row">
+        <div class="col-lg-12">
+            
+
+    
+
+            {!! Form::model($data, [
+                    'method' => 'PATCH', 
+                    'files' => true, 
+                    'action' => ['SettingsController@update', $data],
+                    'class' => 'form-horizontal',
+                ]) !!}
+                
+                <div class="form-group">
+                    {!! Form::label('site_name', 'Site name:', ['class' => 'col-md-2 control-label']) !!}
+                
+                    <div class="col-md-8">
+                        {!! Form::text('site_name', null, ['class' => 'form-control input-md']) !!}
+                        <p class="help-block">help</p>  
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('logo_image', 'Logo image:', ['class' => 'col-md-2 control-label']) !!}
+                
+                    <div class="col-md-8">
+                        {!! Form::text('logo_image', null, ['class' => 'form-control input-md']) !!}
+                        <p class="help-block">help</p>  
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('background_image', 'Background image:', ['class' => 'col-md-2 control-label']) !!}
+                
+                    <div class="col-md-8">
+                        {!! Form::text('background_image', null, ['class' => 'form-control input-md']) !!}
+                        <p class="help-block">help</p>  
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('banner', 'Banner on\off', ['class' => 'col-md-2 control-label']) !!}
+                    <div class="col-md-8">
+                        {!! Form::select('banner', ['No', 'Yes'], null, ['required' => 'required', 'class' => 'form-control']) !!}
+                    </div>
+                </div>
+
+                <button type="submit" class="btn btn-primary center-block">Save</button>
+            {!! Form::close() !!}
+        </div>
+    </div>
+
 @endsection
